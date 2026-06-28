8b147858d1de704dd038b619eacc2f203122c6c5
Aaron Sean Cubacub <aaronseancubacub@gmail.com>
2026-06-28 21:21:04 +1000

    react start

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Foundation\DevCommands;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+
+DevCommands::artisan('serve --host=[::]', 'server');
+DevCommands::node('dev -- --host ::', 'vite');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
